0079ef6f68c83eb940a7619c8e87d65ee50f742c
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2023-03-04 17:54:13 +0300

    EventByTemp

--- JAVA101/EventRecByTempature/src/main/java/org/example/Main.java
+++ JAVA101/EventRecByTempature/src/main/java/org/example/Main.java
@@ -9,6 +9,21 @@ public class Main {
         Scanner scanner=new Scanner(System.in);
         System.out.print("Sıcaklığı giriniz :");
         temp=scanner.nextInt();
-        if ()
+        if (temp<5)
+        {
+            System.out.print("Kayak gidebilirsiniz");
+        }
+        else if(temp>=5 &&temp<15)
+        {
+            System.out.print("Sinema gidebilirsiniz ");
+        }
+        else if(temp>15 && temp<25)
+        {
+            System.out.print("Piknik gidebilirsiniz ");
+        }
+        else if (temp>25)
+        {
+            System.out.print("Yüzme gidebilirsiniz ");
+        }
     }
 }
