c515b8f4f80ddf3d321c2dafac6c39d9e5906bd6
ANIL CAN ÖZDEMİR <anil.ozdemir@avkaryazilim.com.tr>
2023-03-05 15:40:38 +0300

    FindPowersof4and5LessThanEnteredNumber

--- JAVA101/Find Powers of 4 and 5 Less Than Entered Number/src/main/java/org/example/Main.java
+++ JAVA101/Find Powers of 4 and 5 Less Than Entered Number/src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Sayi giriniz      :");
+        int sayi=scanner.nextInt();
+
+        for(int i=1,j=1;i<=sayi || j<=sayi;i*=4,j*=5)
+        {
+            if(i<=sayi)
+                System.out.println("4 ün kuvveti    :"+i);
+            if(j<=sayi)
+                System.out.println("5 ün kuvveti    :"+j);
+        }
     }
 }
