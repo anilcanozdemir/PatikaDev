f1b9af5e235646c62cda74bd5c5c4d64777c0b7b
ANIL CAN ÖZDEMİR <anilcanozdemir1020@gmail.com>
2023-03-05 15:56:22 +0300

    FactorialCalculator

--- JAVA101/FactorialCalculator/src/main/java/org/example/Main.java
+++ JAVA101/FactorialCalculator/src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.print("Eleman Sayisini giriniz     :");
+        Scanner scanner=new Scanner(System.in);
+        int count=scanner.nextInt();
+        System.out.print("Kümedeki Eleman Sayisini giriniz     :");
+        int subCount=scanner.nextInt();
+        int remainderCount=count-subCount;
+        int countComb=1,subCountComb=1,remainderComb=1;
+        for(int i=1;i<count;i++)
+        {
+            countComb*=i;
+            if(i==subCount)
+                subCountComb=countComb;
+            if(i==remainderCount)
+                remainderComb=countComb;
+        }
+        int total=countComb/(subCountComb*remainderComb);
+        System.out.print("Sonuç :" +total);
     }
 }
