b76223ecc85d18058a73daffc02c57fa54d8eef6
ANIL CAN ÖZDEMİR <anil.ozdemir@avkaryazilim.com.tr>
2023-03-05 14:49:39 +0300

    LeapYear

--- JAVA101/LeapYear/src/main/java/org/example/Main.java
+++ JAVA101/LeapYear/src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Yıl giriniz   :");
+        int year=scanner.nextInt();
+        if(year%4==0 && year%100!=0)
+        {
+            System.out.print(year+"bir artık yıldır !");
+        }
+        else if (year%400==0)
+            System.out.print(year+"bir artık yıldır !");
+        else
+            System.out.print(year+"bir artık yıldır değildir !");
     }
 }
